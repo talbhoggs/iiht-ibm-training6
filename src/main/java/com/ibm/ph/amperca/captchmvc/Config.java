fa1dc7971c887a842029ded8c4bfc6c4f526ff62
Charles AMPER <amperca@ph.ibm.com>
2019-11-10 05:47:34 +0800

    add bootstrap 4 webjars

--- src/main/java/com/ibm/ph/amperca/captchmvc/Config.java
+++ src/main/java/com/ibm/ph/amperca/captchmvc/Config.java
@@ -5,19 +5,26 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan("com.ibm.ph.amperca.captchmvc")
-public class Config {
+public class Config implements WebMvcConfigurer {
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/","/resources/");
+	}
 
 	@Bean
-    public ViewResolver viewResolver () {
-        InternalResourceViewResolver viewResolver =
-                  new InternalResourceViewResolver();
-        viewResolver.setPrefix("/WEB-INF/views/");
-        viewResolver.setSuffix(".jsp");
-        return viewResolver;
-    }
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,10 @@
 <html>
+<head>
+<link href="webjars/bootstrap/4.3.1/css/bootstrap.min.css" type="text/css" rel="stylesheet"/>
+</head>
 <body>
 <h2>INDEX PAGE</h2>
+<button type="button" class="btn btn-primary">Oh yeah!</button>
+<script src="webjars/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
